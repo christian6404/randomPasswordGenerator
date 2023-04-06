3de72058899767ebfe44f61b5cd13829238db899
christian6404 <christian6404@gmail.com>
2023-04-05 20:25:22 -0400

    Final
    forgot to commit i was too concentrated
    on actually coding.... finished the script and fixed all the bugs. Probably lost a couple hairs in the mean time.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # randomPasswordGenerator
- A random password generator using javascript
+ A random password generator using javascript made by Christian Smith
+ 
+ # Contact
+ Christian Smith
+ email : christian6404@gmail.com
+ github : https://github.com/christian6404
+
+
+# IMPORTANT
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+If you find any bugs or know of any ways to condense the code please contact me im still very new and would love all the feedback i could get.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ 
+# About
+This was extremely frustrating and taught me alot about JavaScript and the only thing that kept me straight was google.
+I took small breaks and that also helped alot because i would think of soloutions while i was not concentrated on coding.
+
+# Sources 
+https://www.w3schools.com/jsref/met_win_confirm.asp
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+https://thisinterestsme.com/javascript-random-array-value/
+https://stackoverflow.com/questions/19280/code-to-ask-yes-no-question-in-javascript
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+https://www.w3schools.com/js/js_functions.asp
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,70 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+var special = ['!','@','#','$','%','^','&','*','(','',')','<','>','?',':','|'];
+var numeric = ['0','1','2','3','4','5','6','7','8','9'];
+var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var characters = '';
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  var passwordText = document.querySelector("#password"); 
+  passwordText.value = password
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+    console.log("u touched the button!!!")
+    var charLength=prompt("How many characters would you like your password to be?")
+        if (charLength<=7){
+            alert("Password must be atleast 8 characters")
+            return;
+        } else if (charLength>=128){
+            alert("Password must be nomore than 128 characters")
+            return;
+        } else {
+            console.log(charLength)
+        }
+        characters = '';
+
+    const specialChar=confirm("Would you like to include special characters?")
+        if (specialChar == true){
+            characters += special.join('');
+            console.log(characters)
+            console.log(specialChar)
+        }
+
+    const numericChar=confirm("Would you like to include numeric characters?")
+        if (numericChar == true){
+            characters += numeric.join('');
+            console.log(characters)
+            console.log(numericChar)
+
+        }
+    const uppercaseChar=confirm("Would you like to include uppercase characters?")
+        if (uppercaseChar == true){
+            characters += upper.join('');
+            console.log(characters)
+            console.log(uppercaseChar)
+        }
+    const lowercaseChar=confirm("Would you like to include lowercase characters?")
+        if (lowercaseChar == true){
+            characters += lower.join('');
+            console.log(characters)
+            console.log(lowercaseChar)
+        } 
+        if (!specialChar && !numericChar && !uppercaseChar && !lowercaseChar) {
+            alert("You must select atleast one type of character")
+            return '';
+        }
+
+        var password = '';
+        for (let i = 0; i < charLength; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length); // took me a long time to figure this out completely
+            password += characters[randomIndex];
+        }
+        return password;
+        console.log("made by christian smith")
+}
+
+
